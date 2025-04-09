8d93b573abd211cc93bacd7ed97fe0e9c922461f
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-04-09 22:42:21 +0200

    Create AccountCreationPage.svelte

---
+++ packages/client/src/pages/AccountCreationPage.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+  <title>Pawbook signup</title>
+</svelte:head>
+
+<script lang="ts">
+    import LanguageDropdown from "$assets/components/LanguageDropdown.svelte";
+</script>
+
+    <main>
+
+    </main>
+    
+<style lang="scss">
+
+</style>
