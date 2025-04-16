ab38944dc9977afcced9cb31ceb844c6ed27ee40
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-04-16 20:03:39 +0200

    Create Input.test.ts

---
+++ packages/client/src/__tests__/components/Input.test.ts
@@ -0,0 +1,38 @@
+import { test, expect, vi } from "vitest";
+import { render, fireEvent } from "@testing-library/svelte";
+import Input from "$components/Input.svelte";
+
+test("Should render with the good label", () => {
+    const { getByText } = render(Input, { props: { label: "Email", name: "email", type: "email" } });
+    expect(getByText("Email")).toBeInTheDocument();
+});
+
+test("Should render with the good placeholder", () => {
+    const { getByPlaceholderText } = render(Input, { props: { name: "email", type: "email", placeholder:"Your email" } });
+    expect(getByPlaceholderText("Your email")).toBeInTheDocument();
+});
+
+test("Should render with the good type", () => {
+    const { getByLabelText } = render(Input, { props: { label: "Email", name: "email", type: "email" } });
+    const input = getByLabelText("Email") as HTMLInputElement;
+    expect(input).toHaveAttribute("type", "email");
+});
+
+test("Should render with the good id", () => {
+    const { getByLabelText } = render(Input, { props: { label: "Email", name: "email", type: "email" } });
+    const input = getByLabelText("Email") as HTMLInputElement;
+    expect(input).toHaveAttribute("id", "email");
+});
+
+test("Should show and hide the password", async () => {
+    const { getByLabelText, getByRole } = render(Input, { props: { label: "Email", name: "password", type: "password" } });
+    const input = getByLabelText("Email") as HTMLInputElement;
+    expect(input).toHaveAttribute("type", "password");
+
+    const button = getByRole("button", { name: /toggle password visibility/i });
+    await fireEvent.click(button);
+    expect(input).toHaveAttribute("type", "text");
+
+    await fireEvent.click(button);
+    expect(input).toHaveAttribute("type", "password");
+});
