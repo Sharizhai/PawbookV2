2f1ff36da15bf29ce1cf72b83f9a2ee78c6d36c0
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-04-26 08:55:33 +0200

    Create TopContainerInfos + tests

---
+++ packages/client/__tests__/components/post/TopContainerInfo.test.ts
@@ -0,0 +1,34 @@
+import { test, expect, vi } from "vitest";
+import { render, fireEvent } from "@testing-library/svelte";
+import { timeElapsed } from "../../../src/utils/dateUtils";
+import TopContainerInfos from "$components/post/TopContainerInfos.svelte";
+
+test("Should render with the good firstName & lastName", () => {
+    const { getByText } = render(TopContainerInfos, { props: { firstName: "User", lastName: "Name", postCreationDate:new Date("2024-10-14T17:33:11.964Z"), profilePicture:"/paws.png" } });
+
+    expect(getByText("User Name")).toBeInTheDocument();
+});
+
+test("Should render with the good profile picture", () => {
+    const { getByAltText  } = render(TopContainerInfos, { props: { firstName: "User", lastName: "Name", postCreationDate:new Date("2024-10-14T17:33:11.964Z"), profilePicture:"/paws.png" } });
+
+    const imgElement = getByAltText("User Avatar");
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement.getAttribute("src")).toBe("/paws.png");
+});
+
+test("Should use default profile picture when none provided", () => {
+    const { getByAltText } = render(TopContainerInfos, { props: { firstName: "User", lastName: "Name", postCreationDate: new Date("2024-10-14T17:33:11.964Z")} });
+
+    const imgElement = getByAltText("User Avatar");
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement.getAttribute("src")).toBe("/paws.png");
+});
+
+test("Should render with the good date", async () => {
+    const testDate = new Date("2024-10-14T17:33:11.964Z");
+    const expectedFormattedDate = timeElapsed(testDate);
+    const { getByText } = render(TopContainerInfos, { props: { firstName: "User", lastName: "Name", postCreationDate:testDate, profilePicture:"/paws.png" } });
+
+    expect(getByText(expectedFormattedDate)).toBeInTheDocument();
+});
---
+++ packages/client/src/components/post/TopContainerInfos.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import { timeElapsed } from "$utils/dateUtils";
+
+    let { 
+        profilePicture, 
+        firstName, 
+        lastName, 
+        postCreationDate 
+    } : { 
+        profilePicture?: string, 
+        firstName: string, 
+        lastName: string,
+        postCreationDate: Date} = $props();
+
+    let formattedDate = $derived(postCreationDate ? timeElapsed(postCreationDate) : '');
+</script>
+    
+    <div class="postcard-user-infos-container">
+        <img src={profilePicture ? profilePicture : "/paws.png"} alt="User Avatar" class="postcard-user-avatar" />
+        <div class="postcard-user-name-container">
+            <div class="postcard-user-name">{firstName} {lastName}</div>
+            <div class="postcard-user-post-date">{formattedDate}</div>
+        </div>   
+    </div>
+
+<style lang="scss">
+    .postcard-user-infos-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        width: 100%;
+    }
+
+    .postcard-user {
+        &-avatar{
+            width: 3.5rem;
+            height: 3.5rem;
+            border-radius: 50%;
+            margin-right: 0.5rem;
+            border: 1px solid rgba(30, 138, 182, 0.4);
+        }
+
+        &-name {
+            font-size: 0.9rem;
+            font-weight: bold;
+            color: var(--main-text-color);
+        }
+
+        &-post-date {
+            font-size: 0.8rem;
+            color: var(--second-text-color);
+        }
+    }
+</style>
