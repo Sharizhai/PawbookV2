865462a07b6f65dcbe0e58fdb705cd0106eb68b7
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-04-26 10:47:37 +0200

    Update LikeButton.test.ts

--- packages/client/__tests__/components/post/LikeButton.test.ts
+++ packages/client/__tests__/components/post/LikeButton.test.ts
@@ -1,6 +1,5 @@
 import { test, expect, vi } from "vitest";
 import { render, fireEvent } from "@testing-library/svelte";
-import likeIcon from "$assets/icons/posts/like.svg?raw";
 import LikeButton from "$components/post/LikeButton.svelte";
 
 test("Should render with the good number of likes", () => {
